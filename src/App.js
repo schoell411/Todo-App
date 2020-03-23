680600e553e363cf8bcd801d0900f96c1b286364
Kevin Schoell <schoell411@gmail.com>
2020-03-22 21:26:25 -0500

    todo app functional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//components
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+const data = [];
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			tasks: data
+		};
+	}
+
+	addTask = task => {
+		const newTask = {
+			task: task,
+			id: Date.now(),
+			completed: false
+		};
+		this.setState({
+			tasks: [...this.state.tasks, newTask]
+		});
+	};
+
+	toggleCompleted = id => {
+		this.setState({
+			tasks: this.state.tasks.map(task => {
+				if (task.id === id) {
+					return {
+						...task,
+						completed: !task.completed
+					};
+				} else {
+					return task;
+				}
+			})
+		});
+	};
+
+	deleteCompleted = e => {
+		e.preventDefault();
+		this.setState({
+			tasks: this.state.tasks.filter(task => {
+				return !task.completed;
+			})
+		});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Todo App</h1>
+				<div className="todo-form">
+					<TodoForm addTask={this.addTask} />
+				</div>
+				<div className="todo-list">
+					<TodoList
+						tasks={this.state.tasks}
+						toggleCompleted={this.toggleCompleted}
+					/>
+				</div>
+				<button onClick={this.deleteCompleted}>Delete Completed Tasks</button>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			newTask: ""
+		};
+	}
+
+	handleChanges = e => {
+		this.setState({
+			newTask: e.target.value
+		});
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		this.props.addTask(this.state.newTask);
+		this.setState({
+			newTask: ""
+		});
+	};
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label htmlFor="todo">New Task:</label>
+				<input type="text" name="todo" onChange={this.handleChanges} />
+				<button>Submit</button>
+			</form>
+		);
+	}
+}
+
+export default TodoForm;
